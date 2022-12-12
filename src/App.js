fb6910decfa614449a8b73c085ea102ea5761d19
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-12 03:45:52 +0200

    add structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 
+import { FieldSettings } from "@syncfusion/ej2-react-dropdowns";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return (<><p>d</p></>)
+  return (<div>
+    <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+          <TooltipComponent content={'setting'} position='Top'>
+            <button><FieldSettings /></button>
+            
+          </TooltipComponent>
+        </div>
+    </div>
+    </BrowserRouter>
+  </div>)
 }
 
 export default App;
