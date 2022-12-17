2cefefd65f1aab2921a7a2aa8a1cad141df1bccf
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-17 13:10:47 +0200

    navigation

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,8 +11,27 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+    console.log(initialState);
+    console.log("Clicked");
+  };
+  console.log(isClicked);
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
