adaba0117f8edc16d8ae9a4c33f1631535d0fd0e
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-18 23:20:21 +0200

    kanban done

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { KanbanComponent } from "@syncfusion/ej2-react-kanban";
+import {
+  ColumnDirective,
+  ColumnsDirective,
+} from "@syncfusion/ej2-react-kanban/src";
+import React from "react";
+import { Header } from "../components";
+import { kanbanData, kanbanGrid } from "../data/dummy";
 
 const Kanban = () => {
   return (
-    <div>Kanban</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title={"Kanban"} category="App" />
 
-export default Kanban
+      <KanbanComponent
+        id="kanban"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+        keyField="Status"
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
+};
+
+export default Kanban;
