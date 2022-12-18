02b0f6d3176c8103cca2517a06556cd96769a580
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-18 23:26:49 +0200

    update Editor title

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -14,7 +14,7 @@ import { EditorData } from "../data/dummy";
 const Editor = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header title={"Kanban"} category="App" />
+      <Header title={"Editor"} category="App" />
       <RichTextEditorComponent>
         <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
       </RichTextEditorComponent>
