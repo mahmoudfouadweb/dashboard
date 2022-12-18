135b89797a01a15c7d501a39a941876d559fbc4f
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-18 23:25:35 +0200

    editor done

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Inject } from "@syncfusion/ej2-react-charts/src";
+import { RichTextEditorComponent } from "@syncfusion/ej2-react-richtexteditor";
+import {
+  HtmlEditor,
+  Image,
+  Link,
+  QuickToolbar,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor/src";
+import React from "react";
+import { Header } from "../components";
+import { EditorData } from "../data/dummy";
 
 const Editor = () => {
   return (
-    <div>Editor</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title={"Kanban"} category="App" />
+      <RichTextEditorComponent>
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
+};
 
-export default Editor
+export default Editor;
