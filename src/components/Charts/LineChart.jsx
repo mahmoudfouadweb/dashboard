dfcda1f25cc673e7d2c97604c54acd1d83decbf5
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-19 04:41:23 +0200

    lineChart done

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Tooltip } from "@syncfusion/ej2-charts/src";
+import {
+  ChartComponent,
+  Inject,
+  DateTime,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+} from "@syncfusion/ej2-react-charts";
+import { LineSeries } from "@syncfusion/ej2/charts";
+
+import React from "react";
+import { useStateContext } from "../../contexts/ContextProvider";
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
 
 const LineChart = () => {
+  const { currentMode, currentColor } = useStateContext();
   return (
-    <div>LineChart</div>
-  )
-}
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373e" : "#fff"}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
 
-export default LineChart
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
+
+export default LineChart;
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Header, LineChart } from "../../components";
 
 const Line = () => {
   return (
-    <div>Line</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-2 bg-white rounded-3xl dark:bg-secondary-dark-bg">
+      <Header title={"Inflatoin Rate"} category="Charts" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  );
+};
 
-export default Line
+export default Line;
