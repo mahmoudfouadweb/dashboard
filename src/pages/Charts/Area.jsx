01b9e1bd926a95c7db3a6999b7a72fb8f8124ab4
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-19 04:59:58 +0200

    update

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Tooltip } from "@syncfusion/ej2-charts/src";
+import {
+  ChartComponent,
+  Inject,
+  DateTime,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+} from "@syncfusion/ej2-react-charts";
+import { LineSeries } from "@syncfusion/ej2/charts";
+
+import React from "react";
+import { Header } from "../../components";
+import { useStateContext } from "../../contexts/ContextProvider";
+import {
+  areaPrimaryXAxis,
+  areaPrimaryYAxis,
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
 
 const Area = () => {
+  const { currentMode, currentColor } = useStateContext();
   return (
-    <div>Area</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-2 bg-white rounded-3xl">
+      <Header title={"Aria Chart"} category="Chart" />
 
-export default Area
+      <ChartComponent
+        id="aria-chart"
+        height="420px"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373e" : "#fff"}
+      >
+        <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+
+        <SeriesCollectionDirective>
+          {lineCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+};
+
+export default Area;
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,15 +1,47 @@
-import React from "react";
-import { Header, LineChart } from "../../components";
+import { Tooltip } from "@syncfusion/ej2-charts/src";
+import {
+  ChartComponent,
+  Inject,
+  DateTime,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+} from "@syncfusion/ej2-react-charts";
+import { LineSeries } from "@syncfusion/ej2/charts";
 
-const Line = () => {
+import React from "react";
+import { Header } from "../../components";
+import { useStateContext } from "../../contexts/ContextProvider";
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
+
+const LineChart = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-2 bg-white rounded-3xl dark:bg-secondary-dark-bg">
-      <Header title={"Inflatoin Rate"} category="Charts" />
-      <div className="w-full">
-        <LineChart />
-      </div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-2 bg-white rounded-3xl">
+      <Header title={"Line Chart"} category="Chart" />
+
+      <ChartComponent
+        id="line-chart"
+        height="420px"
+        primaryXAxis={LinePrimaryXAxis}
+        primaryYAxis={LinePrimaryYAxis}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373e" : "#fff"}
+      >
+        <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+
+        <SeriesCollectionDirective>
+          {lineCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
     </div>
   );
 };
 
-export default Line;
+export default LineChart;
