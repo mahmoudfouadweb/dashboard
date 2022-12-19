ffdeb4f16b8041311bb1c4ad13301997125932e2
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-19 20:23:29 +0200

    bar chart done

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  Tooltip,
+  DataLabel,
+  ColumnSeries,
+} from "@syncfusion/ej2-react-charts";
+import { Header } from "../../components";
+import {
+  barChartData,
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
 
 const Bar = () => {
   return (
-    <div>Bar</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-2 bg-white rounded-3xl">
+      <Header title={"Bar"} category="Chart" />
+      <ChartComponent
+        id="bar-chart"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        dataSource={barChartData}
+        legendSettings={{ visible: true, position: "Top" }}
+      >
+        <Inject
+          services={[ColumnSeries, Legend, Tooltip, DataLabel, Category]}
+        />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, idx) => (
+            <SeriesDirective key={idx} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+};
 
-export default Bar
+export default Bar;
