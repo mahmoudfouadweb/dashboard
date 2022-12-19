3c53dfcf43198297483d8409c02b8372ffada98d
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-19 20:37:56 +0200

    pieChart done

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+} from "@syncfusion/ej2-react-charts";
+import { Header } from "../../components";
+import { pieChartData } from "../../data/dummy";
 
 const Pie = () => {
   return (
-    <div>Pie</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-2 bg-white rounded-3xl">
+      <Header title={"Pie Doughnut"} category="Chart" />
+      <AccumulationChartComponent id={"pie-chart"}>
+        <AccumulationSeriesCollectionDirective>
+          <AccumulationSeriesDirective
+            dataSource={pieChartData}
+            xName="x"
+            yName="y"
+          />
+        </AccumulationSeriesCollectionDirective>
+      </AccumulationChartComponent>
+    </div>
+  );
+};
 
-export default Pie
+export default Pie;
