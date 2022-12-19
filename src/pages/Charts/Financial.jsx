aacbea61c3229b3a623f6b8bc90c7ee5f3c34b20
Mahmoud Fouad <mfapolice@gmail.com>
2022-12-19 21:04:40 +0200

    done finance

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../../components";
+import {
+  AxisModel,
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  LegendSettingsModel,
+  HiloSeries,
+  Category,
+  Tooltip,
+  ILoadedEventArgs,
+  Zoom,
+  Crosshair,
+  ChartTheme,
+} from "@syncfusion/ej2-react-charts";
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from "../../data/dummy";
 
-const Finacial = () => {
+const Financial = () => {
   return (
-    <div>Finacial</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-2 bg-white rounded-3xl">
+      <Header title={"Financial"} category="Chart" />
 
-export default Finacial
+      <ChartComponent
+        id="charts"
+        primaryXAxis={FinancialPrimaryXAxis}
+        primaryYAxis={FinancialPrimaryYAxis}
+        title="Maximum and Minimum Rainfall"
+      >
+        <Inject services={[HiloSeries, Tooltip, Category, Crosshair, Zoom]} />
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={financialChartData}
+            xName="x"
+            yName="low"
+            name="Egypt"
+            type="Hilo"
+            low="low"
+            high="high"
+          ></SeriesDirective>
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+};
+
+export default Financial;
